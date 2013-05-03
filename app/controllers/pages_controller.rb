8bb0e661139bb100d032a15fefb4cbff51a6a148
dbc gogo <dbc@dbc.com>
2013-05-02 22:56:45 -0700

    Developing instagram client auth logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
-require_relative "../../lib/InstagramApi"
+require_relative "../../lib/InstagramAPI"
 
 class PagesController < ApplicationController
 
-  include InstagramApi
+  include InstagramAPI
 
   def index
     # puts "i'm in here"
     # t = Instagram.user_search("armynante").first
 
     # Post.create(username: t.full_name, image_url: t.profile_picture)
-    InstagramApi.user("armynante")
+    InstagramAPI.user("armynante")
+
+    render :nothing => true
   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :resource
-  attr_accessible :body, :media_type, :posted_at, :title, :url
+
+  attr_accessible :body, :media_type, :posted_at, :title, :url, :caption
+
+  alias_attribute :caption, :title
 end
