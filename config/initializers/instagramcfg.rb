edbf5d88841a70b31d3c2d680a35985bfa1a7a99
dbc gogo <dbc@dbc.com>
2013-05-02 22:56:45 -0700

    Developing instagram client auth logic

--- config/initializers/instagramcfg.rb
+++ config/initializers/instagramcfg.rb
@@ -1,6 +1,10 @@
-# CALLBACK_URL = "http://localhost:9393/oauth/callback"
+CALLBACK_URL = "http://localhost:9393/oauth/callback"
+access_token = "357627834.3bdcc20.299991d0a5b24034bae7bc68f189e6a0"
+
 require 'instagram'
 Instagram.configure do |config|
   config.client_id = "3bdcc204c16b4beea31ae981d9fefbab"
   config.client_secret = "98aaf7f9d3d2472ebe18a1bc553f116f"
 end
+
+client = Instagram.client(:access_token => access_token)
--- lib/InstagramApi.rb
+++ lib/InstagramApi.rb
@@ -1,14 +1,21 @@
-module InstagramApi
+module InstagramAPI
   extend self
-
+  DBCSF_lat = "37.79221"
+  DBCSF_long = "-122.406141"
 
   def user(username)
     t = Instagram.user_search(username).first
-
-    Post.create(username: t.full_name, image_url: t.profile_picture)
+    Resource.find(2).posts.create(title: t.full_name, media_type: 'photo', posted_at: DateTime.now )
   end
+#find(2) = "find_by_username(username)..."
+  def geolocation_search
+    Instagram.media_search(DBCSF_lat, DBCSF_long).each do |obj|
+    Resource.find(2).posts.create( caption: obj.caption.text,
+                                   media_type: 'photo',
+                                   posted_at: DateTime.strptime(obj.created_time, '%s'),
+                                   url: obj.images.standard_resolution.url
+                                  )
+  end 
 
+#find(2) = same story...
 end
-
-
-
