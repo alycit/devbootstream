9c9451e9e49e984d2c721f659ea0558029506462
Johny Ho <jho406@gmail.com>
2013-05-04 12:04:07 -0700

    fixed worker to account for unique posts

--- app/workers/tumblr_worker.rb
+++ app/workers/tumblr_worker.rb
@@ -7,13 +7,18 @@ class TumblrWorker
     client = Tumblr::Client.new
 
     resources.each do |resource|
-      puts resource.identifier
-      data = client.posts(resource.identifier)
+      #todo: refactor
+      offset = 0
+      save = true
 
-      unless data["status"] == 404
+      while save
+        data = client.posts(resource.identifier)
+        break if data["status"] == 404
+        offset = data["posts"].length
+
+        puts resource.identifier
+        save = false
         data["posts"].each do |post|
-          #todo: need to account for repeat posts and new posts, but will have 
-          #to wait for agreed validation
           resource.posts.new(
             :media_type=>post["type"],
             :url=>post["url"],
@@ -22,9 +27,10 @@ class TumblrWorker
             :posted_at => DateTime.strptime(post["timestamp"].to_s,"%s"),
             :data => post
           )
-
-          resource.save
+          save = resource.save
+          offset = 0 unless save
         end
+
       end
     end
 
