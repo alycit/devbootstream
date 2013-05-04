8fb41fe090b6024140f051cf2ecf9738dd87d6c5
dbc gogo <dbc@dbc.com>
2013-05-03 18:55:16 -0700

    removed old instagram data

--- config/initializers/instagramcfg.rb
+++ config/initializers/instagramcfg.rb
@@ -1,12 +1,4 @@
-CALLBACK_URL = "http://localhost:9393/oauth/callback"
-
-access_token = "357627834.3bdcc20.299991d0a5b24034bae7bc68f189e6a0"
-
-require 'instagram'
 Instagram.configure do |config|
-  config.client_id = "3bdcc204c16b4beea31ae981d9fefbab"
-  config.client_secret = "98aaf7f9d3d2472ebe18a1bc553f116f"
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 end
-
-client = Instagram.client(:access_token => access_token)
-
