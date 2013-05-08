22c40e65202d0fe768863d1253452a3c780e0a38
dbc gogo <dbc@dbc.com>
2013-05-07 17:54:04 -0700

    Created initial tests

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Cohort do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    cohort = Cohort.new(name: "Golden Bears", start_date: DateTime.now, socrates_cohort_id: 5)
+    cohort.save!(:validate => false)
+
+    it "passes validations for presence and uniqueness of 'name'" do
+      should validate_uniqueness_of(:name)
+      should validate_presence_of(:name)
+    end
+  end
+
+  describe "associations" do
+    it { should have_many(:boots) }
+  end
+
+  describe "#initialize" do
+    it { should be_an_instance_of(Cohort) }
+  end
+
 end
